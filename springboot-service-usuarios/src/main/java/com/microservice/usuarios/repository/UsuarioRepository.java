8f2752c56ee34145ceeb19b8b87b1e233e1541e2
hvanjerdz <hvanjerdz@proton.me>
2023-12-01 17:17:37 -0600

    Agrega RRP

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -2,7 +2,9 @@ package com.microservice.usuarios.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.microservice.usuarios.entity.Usuario;
 
@@ -10,7 +12,8 @@ import com.microservice.usuarios.entity.Usuario;
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
 	
 	//select u from Usuario u where u.username = ?1
-	public Usuario findByUsername(String username);
+	@RestResource(path = "buscar-username")
+	public Usuario findByUsername(@Param("nombre") String username);
 	
 	//select u from Usuario u where u.username = ?1 and u.email = ?2
 	public Usuario findByUsernameAndEmail(String username, String email);
